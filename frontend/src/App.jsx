dd638d61e192bcf8457216c305c12876750f99cc
Syed Talha <syedtalhadev7@gmail.com>
2025-01-05 00:32:26 +0500

    added new links - replaced favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Collection from "./pages/Collection";
+import Brands from "./pages/Brands";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
@@ -18,7 +18,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
+        <Route path="/brands" element={<Brands />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product/:productId" element={<Product />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,11 +6,29 @@ const Navbar = () => {
     <div className="flex items-center justify-between py-5 font-medium">
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-        <NavLink className="flex flex-col items-center gap-1">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
           <p className="text-black">خانه</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
+
+        <NavLink to="/brands" className="flex flex-col items-center gap-1">
+        <p className="text-black">برندها</p>
+        <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p className="text-black">درباره</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p className="text-black">ارتباط</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
       </ul>
+
+      
       <img src={logo} className="w-36" alt="Roonac Logo" />
     </div>
   );
